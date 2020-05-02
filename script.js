7e78b5928387abbe4f22c7e2683fabb447796dbc
Aoliabe Jacques <aolibjacques@gmail.com>
2020-05-02 15:25:14 -0400

    ceto

---
+++ script.js
@@ -0,0 +1,48 @@
+function somar() {
+    
+    // Pegar valores dos inputs
+    let numero1 = document.getElementById("numero1").value
+    let numero2 = document.getElementById("numero2").value
+
+    // Somar os dois valores
+    let resultado = parseFloat(numero1) + parseFloat(numero2)
+
+    // Imprimir o resultado (alertbox)
+    alert(resultado)
+}
+function dividir() {
+
+    // Pegar valores dos inputs
+    let numero1 = document.getElementById("numero1").value
+    let numero2 = document.getElementById("numero2").value
+
+    // Somar os dois valores
+    let resultado = parseFloat(numero1) / parseFloat(numero2)
+
+    // Imprimir o resultado (alertbox)
+    alert(resultado)
+} 
+function subitrair() {
+
+    // Pegar valores dos inputs
+    let numero1 = document.getElementById("numero1").value
+    let numero2 = document.getElementById("numero2").value
+
+    // subitrai os dois valores
+    let resultado = parseFloat(numero1) - parseFloat(numero2)
+
+    // Imprimir o resultado (alertbox)
+    alert(resultado)
+} 
+function multiplicar() {
+
+    // Pegar valores dos inputs
+    let numero1 = document.getElementById("numero1").value
+    let numero2 = document.getElementById("numero2").value
+
+    // Somar os dois valores
+    let resultado = parseFloat(numero1) * parseFloat(numero2)
+
+    // Imprimir o resultado (alertbox)
+    alert(resultado)
+}
